a2d12756f1964270841d14d467e8c8f80c196d1c
Isabelar07 <isabelarochas@outlook.com.br>
2020-09-29 16:34:22 -0300

    exercicios de operadores-js

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,74 @@
+/*Exercicio de interpretação:
+
+1) 
+
+valor booleano
+valor booleano
+o valor é o oposto da constante 2
+
+imprime no console:
+
+a. false
+b. false
+c. true. 
+e. valor booleano
+
+2) imprime no console:
+
+a. undefined, pois a constante não tem nenhum valor.
+b. null
+c.tamanho do array
+d. 3
+e. troca o valor do segundo numero por 19
+f. 9
+*/
+
+/* exercicios de escrita de código
+
+1) 
+
+
+let idade = prompt("Qual a sua idade?")
+console.log(idade)
+
+let idadeMelhorAmigo = prompt("Qual a idade do seu melhor amigo?")
+console.log(idadeMelhorAmigo)
+
+let maior = Number(idade) > Number(idadeMelhorAmigo)
+let diferenca = Number(idade) - Number(idadeMelhorAmigo)
+console.log(diferenca)
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", "false")
+
+2) 
+
+let numeroPar = prompt("Insira um número par!")
+let converter = Number(numeroPar)
+console.log(numeroPar%0)
+
+// testei vários numeros pares e numeros pares nao sobram resto da divisão
+
+// se inserir um numero ímpar o numero vai ser impresso no console, assim como foi os numeros pares, pois o programa não esta programado para rejeitar números impares
+
+3)  
+
+let listaDeTarefas = []
+let tarefa = prompt("Qual tarefa você precisa realizar hoje?")
+listaDeTarefas.push("assistir aula")
+listaDeTarefas.push("fazer atividade")
+listaDeTarefas.push("fazer desafio")
+
+console.log(listaDeTarefas)
+
+let indiceTarefa = Number(prompt("Digite o indice da tarefa que você ja realizou usando 0, 1 ou 2"))
+listaDeTarefas.splice(indiceTarefa, 1)
+
+console.log(listaDeTarefas)
+
+4) 
+
+let nome = prompt("Qual o seu nome?")
+let email = prompt("Qual o seu e-mail?")
+
+console.log(`O email ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`)
+*/
