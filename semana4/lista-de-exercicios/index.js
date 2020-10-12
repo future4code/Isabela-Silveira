5151743af0f1e473b05e59fe4beb48e77eece3ed
Isabelar07 <isabelarochas@outlook.com.br>
2020-10-12 17:35:56 -0300

    primeiro exercicio de logica de programacao, percorri a lista usando while, for e for..of

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,52 @@
+/* EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO: 
+
+1) Foi criada uma função para converter moeda. Essa função retorna o resultado da multiplicação do valor em dolar e da cotação do dolar, que foi pedido no prompt, em reais. O valor do Dolár foi estabelicido ao chamar a função na const meuDinheiro. 
+
+2) Essa é uma função que calcula investimentos, nela tem todos os tipos de investimento e tem a multiplicação do valor pelos tipos de investimento. A const novoMontante está chamando a função e estabelecendo o tipo de investimento - Ações e o valor, 150, que vai ser estabelecido. Como o tipo de investimento está listado no switch  vai seer multiplicado o valor 150 por 1.1. Já a const segundoMontante vai ser direcionada para o default, pois Tesouro direto não está listado na case e será emitido um alerta informando que o tipo de investimento informado é incorreto.
+
+3) Nesse o if pega os números do array numeros, se o número for par ele é adicionado no array1, se for ímpar é adicionado no array2. No primeiro console.log é impresso no console o tamanho do array numeros, no segundo console.log é impresso o tamanho do array1, assim como também é impresso no terceiro console.log o tamanho do array2.
+
+4) Nesse se o numero do array numeros for menor que numero1, esse número será colocado dentro da variável numero1. Caso o numero seja maior será colocado dentro da variável numero2. Por fim, o primeiro console.log imprime a variável numero1 e o segundo a variável numero2.
+
+----------------------------------------------------------------------------------------------------------------------
+
+EXERCÍCIO DE LÓGICA DE PROGRAMAÇÃO: 
+
+
+// 1) Programa para percorrer/iterar uma lista
+
+1- 
+let lista = ["doritos", "ruffles", "açaí", "chocolate"]
+let i = 0
+
+while(i < lista.length) {
+    console.log(`${lista[i]}`)
+    i++
+}
+
+2-
+let lista = ["doritos", "ruffles", "açaí", "chocolate"]
+
+for(let i = 0; i < lista.length; i++) {
+    console.log(lista[i])
+}
+
+3-
+let lista = ["doritos", "ruffles", "açaí", "chocolate"]
+
+for(let comida of lista) {
+    console.log(comida)
+}
+
+----------------------------------------------------------------------------------------------------------------------
+
+*/ 
+
+
+
+
+
+
+
+
+
