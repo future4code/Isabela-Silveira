4051d798fedcd2a5a8b783ca7f9e5d5541dd24c8
Isabelar07 <isabelarochas@outlook.com.br>
2020-10-19 19:56:44 -0300

    tentei fazer a atividade de renderização condicional - labeforms

---
+++ semana6/labeforms/labe-forms/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {DadosGerais} from './components/dadosgerais.js';
+
+function App() {
+  return (
+    <div className='dados'>
+      <DadosGerais />
+    </div>
+  );
+}
+
+export default App;
