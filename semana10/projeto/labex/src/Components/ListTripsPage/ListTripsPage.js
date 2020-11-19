b799f204c0c83d5dd72abc243732b3fbe1b7709a
Isabelar07 <isabelarochas@outlook.com.br>
2020-11-19 15:36:11 -0300

    peguei a lista de viagem da API

--- semana10/projeto/labex/src/Components/ListTripsPage/ListTripsPage.js
+++ semana10/projeto/labex/src/Components/ListTripsPage/ListTripsPage.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios';
+import { LisTripContainer } from './styled'
 
 function ListTripsPage () {
+    const [trip, setTrip] = useState([])
+
+    useEffect(() => {
+        getTrip()
+    }, [])
+
+    const getTrip = () => {
+        Axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labeX/isabela-dumont/trips').then((res) => {
+            setTrip(res.data.trips)
+        }).catch((err) => {
+            alert('Erro: ', err.message)
+        })
+    }
+    
+    const renderListTrip = trip.map((trips) => {
+        return (
+
+        <LisTripContainer>
+            
+            <p>{trips.name}</p>
+            <p>{trips.description}</p>
+            <p>{trips.planet}</p>
+            <p>{trips.durationInDays}</p>
+            <p>{trips.date}</p>
+            <button>Quero Ir</button>
+
+        </LisTripContainer>
+
+        )
+        
+    })
+
     return (
         <div>
-            <h1>Esse é da lista de viagens</h1>
+        
+            <div>
+                {renderListTrip}
+            </div>
+            
         </div>
     )
 }
---
+++ semana10/projeto/labex/src/Components/ListTripsPage/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const LisTripContainer = styled.div`
+    border: 2px solid black;
+    width: 200px;
+    margin: auto;
+    padding: 20px;
+    text-align: center;
+
+`
