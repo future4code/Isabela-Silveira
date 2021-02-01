feaa22e9465dc939e4b954e2cef967d109b1a9a8
Isabelar07 <isabelarochas@outlook.com.br>
2021-02-01 16:31:42 -0300

    signup

---
+++ semana19/aula54/arq-software/src/business/userBusiness.ts
@@ -0,0 +1,37 @@
+import { insertUser } from "../data/userDataBase";
+import { USER_ROLES } from "./entities/user";
+import { generateToken } from "./services/authenticator";
+import { hash } from "./services/hashManager";
+import { generateId } from "./services/idGenerator";
+
+export const businessSignup = async(
+   name: string,
+   email: string,
+   password: string,
+   role: USER_ROLES
+) => {
+    
+    if (!name || !email || !password || !role) {
+        throw new Error('Preencha os campos "name", "email", "password" e "role"')
+    }
+
+    const id: string = generateId()
+
+    const cypherPassword = await hash(password)
+
+    await insertUser({
+        id,
+        name,
+        email,
+        password: cypherPassword,
+        role
+    })
+
+    const token: string = generateToken({
+        id,
+        role
+    })
+
+    return token 
+
+}
---
+++ semana19/aula54/arq-software/src/data/userDataBase.ts
@@ -0,0 +1,14 @@
+import { user } from "../business/entities/user";
+import { connection } from "./connection";
+
+export const insertUser = async(
+    user: user
+) => {
+    await connection.insert({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        role: user.role
+    }).into('User_Arq')
+}
