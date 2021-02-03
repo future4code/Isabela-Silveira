00bae26ac8a4dfdef41a3b346cfec69bab4c03f9
Isabelar07 <isabelarochas@outlook.com.br>
2021-02-03 17:16:00 -0300

    coloquei o gerador de id na pasta services

--- semana19/projeto/labook/src/index.ts
+++ semana19/projeto/labook/src/index.ts
@@ -5,7 +5,7 @@ import cors from "cors"
 
 import * as jwt from "jsonwebtoken"
 import * as bcrypt from "bcryptjs"
-import { v4 } from "uuid"
+
 
 
 /**************************** CONFIG ******************************/
@@ -19,7 +19,7 @@ app.use(cors())
 
 /**************************** SERVICES ******************************/
 
-const generateId = (): string => v4()
+
 
 function generateToken(
    payload: AuthenticationData
