3ff606a17e6bdd50f68f20e260e940c66c6545f2
Isabelar07 <isabelarochas@outlook.com.br>
2021-02-09 16:43:32 -0300

    exercício 1

--- semana20/aula59/testes-unitarios-e-mocks/src/ex1.ts
+++ semana20/aula59/testes-unitarios-e-mocks/src/ex1.ts
@@ -3,3 +3,22 @@
    defesa,
    força
 */
+
+interface persons {
+    name: string,
+    life: number,
+    defense: number,
+    force: number
+}
+
+export const validateCharacter = (input: persons): boolean => {
+    if (!input.name || input.life === undefined ||input.defense === undefined || input.force === undefined) {
+        return false
+    }
+
+    if (input.life <= 0 || input.defense <= 0 || input.force) {
+        return false
+    }
+
+    return true
+} 
